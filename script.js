68fdf1fb0feb3fba0ed7b89a0bced243282c78be
anthonyochoa99 <aeochoa99@outlook.com>
2025-07-25 16:32:44 -0700

    Add a function that generates cards for the game
    - Generates 50 test cards and appends them
    to the game-board section.

--- script.js
+++ script.js
@@ -27,7 +27,19 @@ const wordBank = ['Lion', 'Lion',
 
 const startBtn = document.querySelector('.start-btn');
 startBtn.addEventListener('click', disableStartBtn);
+startBtn.addEventListener('click', generateCards);
 
 function disableStartBtn() {
     startBtn.disabled = true;
+}
+
+function generateCards() {
+    let gameBoard = document.querySelector('.game-board');
+
+    for (let i = 1; i < 51; i++) {
+        let card = document.createElement('div');
+        card.classList.add(`card-${i}`);
+        card.innerHTML = 'test';
+        gameBoard.appendChild(card);
+    }
 }
